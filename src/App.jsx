0075f8aa75955f0d5590b895745ddb087d9aca6e
dani <danifromecuador@gmail.com>
2023-06-20 23:23:40 -0500

    Create a Navbar and 3 empty pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-
+import Navbar from './components/Navbar';
 function App() {
 
   return (
     <div className="App">
-      hello dani
+      <Navbar />
     </div>
   )
 }
---
+++ src/pages/Calculator.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+const Calculator = () => {
+  return (
+    <div>Calculator</div>
+  )
+}
+
+export default Calculator
