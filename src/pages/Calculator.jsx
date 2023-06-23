3b32bc69f0984d5be6b4c22744a40c9052609bdf
dani <danifromecuador@gmail.com>
2023-06-23 12:33:00 -0500

    Avoid input large numbers, the user just can input 10 or less  digits numbers

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -7,11 +7,12 @@ const Calculator = () => {
   const [previousResult, setPreviousResult] = useState('0');
   const [operation, setOperation] = useState('');
   const [result, setResult] = useState('0');
-  
+
   const numberBtn = (event) => {
-    if (result === '0') setResult(event.target.value);
-    else setResult(result + event.target.value);
-    console.log(result);
+    if (result.length < 10) { //I'm using this condition to avoid large numbers
+      if (result === '0') setResult(event.target.value);
+      else setResult(result + event.target.value);
+    }
   };
 
   const dotBtn = () => {
@@ -55,46 +56,45 @@ const Calculator = () => {
 
   const equalBtn = () => {
     if (operation === '+') setResult((parseFloat(previousResult) + parseFloat(result)).toString());
-    else if (operation === '-') setResult((parseFloat(previousResult) - parseFloat(result)).toString());
-    else if (operation === '*') setResult((parseFloat(previousResult) * parseFloat(result)).toString());
-    else if (operation === '/') setResult((parseFloat(previousResult) / parseFloat(result)).toString());
-    
-  };  
+    if (operation === '-') setResult((parseFloat(previousResult) - parseFloat(result)).toString());
+    if (operation === '*') setResult((parseFloat(previousResult) * parseFloat(result)).toString());
+    if (operation === '/') setResult((parseFloat(previousResult) / parseFloat(result)).toString());
+  };
 
   return (
     <div className="calculator-container">
-      <Display result={result}/>
+      <Display result={result} />
       <div className="buttons">
         <div className="row">
-          <Button symbol="AC" onClick= {acBtn} aditionalClassName="AC"/>
-          <Button symbol="%"/>
-          <Button symbol="+/-"/>
-          <Button symbol="DEL" onClick={delBtn} aditionalClassName="DEL"/>
+          <Button symbol="AC" onClick={acBtn} aditionalClassName="AC" />
+          <Button symbol="%" />
+          <Button symbol="+/-" />
+          <Button symbol="DEL" onClick={delBtn} aditionalClassName="DEL" />
         </div>
 
         <div className="row">
           <Button symbol="7" onClick={numberBtn} />
           <Button symbol="8" onClick={numberBtn} />
           <Button symbol="9" onClick={numberBtn} />
-          <Button symbol="&divide;" onClick={divideBtn}/>
+          <Button symbol="&divide;" onClick={divideBtn} />
         </div>
         <div className="row">
           <Button symbol="4" onClick={numberBtn} />
           <Button symbol="5" onClick={numberBtn} />
           <Button symbol="6" onClick={numberBtn} />
-          <Button symbol="&times;" onClick={multiplyBtn}/>
+          <Button symbol="&times;" onClick={multiplyBtn} />
         </div>
         <div className="row">
           <Button symbol="1" onClick={numberBtn} />
           <Button symbol="2" onClick={numberBtn} />
           <Button symbol="3" onClick={numberBtn} />
-          <Button symbol="&minus;" onClick={subtractBtn}/>
+          <Button symbol="&minus;" onClick={subtractBtn} />
         </div>
         <div className="row">
           <Button symbol="0" onClick={numberBtn} />
           <Button symbol="&middot;" onClick={dotBtn} />
-          <Button symbol="=" onClick={equalBtn}/>
-          <Button symbol="+" onClick={addBtn}/>
+          <Button symbol="=" onClick={equalBtn} />
+          <Button symbol="+" onClick={addBtn} />
         </div>
       </div>
     </div>
