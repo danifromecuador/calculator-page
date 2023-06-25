8db1b97b9713cc6af4a214ab36c21d0e7dd9cd13
dani <danifromecuador@gmail.com>
2023-06-25 12:16:24 -0500

    Fetch the quote from API using async and await

--- src/pages/Quote.jsx
+++ src/pages/Quote.jsx
@@ -1,16 +1,26 @@
 import '../styles/Quote.css';
 import { useState } from 'react';
-
 const Quote = () => {
-  const [somedata, resetSomedata] = useState('nel');
-  fetch('https://api.api-ninjas.com/v1/quotes?category=happiness', {
-    headers: { 'X-Api-Key': 'nezpEokeOVTwP1C5NwtGEQ==h62LjJXfJNexoQfy'},
-    contentType: 'application/json'
-  })
-  .then(response => response.json())
-  .then(data => console.log(data))
+  const [quoteString, setQuoteString] = useState("no changes");
+  const handleClick = async () => {
+    const response = await fetch('https://api.api-ninjas.com/v1/quotes?category=happiness', {
+      headers: { 'X-Api-Key': 'nezpEokeOVTwP1C5NwtGEQ==h62LjJXfJNexoQfy' }
+    })
+    const data = await response.json();
+    const quote = data[0].quote;
+    setQuoteString(quote);
+  };
+
+
+
   return (
-    <div className="quote-container">holiwis</div>
+    <div className="quote-container">holiwis
+      <button type="button" onClick={handleClick} className='quoteBtn'>Get your daily quote!!!</button>
+      <p>
+        {quoteString}
+      </p>
+
+    </div>
   );
 }
 
