1d2f41c6a58698a207a6e2c1c059589b7bf9474d
dani <danifromecuador@gmail.com>
2023-06-25 12:56:00 -0500

    Render a quote and it's author

--- src/pages/Quote.jsx
+++ src/pages/Quote.jsx
@@ -1,27 +1,31 @@
 import '../styles/Quote.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
 const Quote = () => {
-  const [quoteString, setQuoteString] = useState("no changes");
-  const handleClick = async () => {
+  const [quote, setQuote] = useState('no quote');
+  const [author, setAuthor] = useState('no author');
+  
+  const fetchQuote = async () => {
     const response = await fetch('https://api.api-ninjas.com/v1/quotes?category=happiness', {
       headers: { 'X-Api-Key': 'nezpEokeOVTwP1C5NwtGEQ==h62LjJXfJNexoQfy' }
-    })
+    });
     const data = await response.json();
-    const quote = data[0].quote;
-    setQuoteString(quote);
+    const {quote, author} = data[0];
+    setQuote(quote);
+    setAuthor(author);
   };
 
-
+  useEffect(() => {
+    console.log("Component rendered");     
+      fetchQuote();
+  }, []);
 
   return (
-    <div className="quote-container">holiwis
-      <button type="button" onClick={handleClick} className='quoteBtn'>Get your daily quote!!!</button>
-      <p>
-        {quoteString}
-      </p>
-
+    <div className="quote-container">
+      <p>{quote}</p>
+      <p>{author}</p>
     </div>
   );
-}
+};
 
 export default Quote;
